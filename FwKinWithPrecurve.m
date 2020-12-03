97d1602d1c77fb65d9ea8cbc3d7b3380098b467e
nepacheco <nepacheco@wpi.edu>
2020-12-02 21:07:47 -0500

    Added section in fwkinwithPrecurve with gradient descent on elstic modulus

--- FwKinWithPrecurve.m
+++ FwKinWithPrecurve.m
@@ -1,4 +1,4 @@
-clc; clear; close all;
+clear; close all;
 % TUBE PARAMETERS %
 n = 5;
 ro = (1.62E-3)/2; % [mm]
@@ -8,7 +8,7 @@ g = 1.4E-3; % [mm]
 c = 2.2085E-3; % [mm]
 E = 40E9; % [Pa]
 mu = 0.2;
-F = 2; % [N] input Force
+F = 1; % [N] input Force
 theta_vec_base = deg2rad(2)*ones(n,1);
 
 % INITIALIZATION OF VECTORS %
@@ -25,11 +25,41 @@ maxTrials = 10;
 while theta_diff > 1E-6 && trials <= maxTrials
     for ii = 1:n
         E_vec(ii)=E;
-                        
+        
         % Compute distal force and moment
         F_vec(ii) = F*exp(-mu*sum(theta_vec(1:ii)));
         M_vec(ii) = F_vec(ii)*(ybar +ri);
         theta_vec(ii) = M_vec(ii)*h/(E_vec(ii)*I);
+        
+        pct = 100;
+        k = 1;
+        % Gradient descent for non-linear modulus
+        while k<100 && pct>1E-4
+            
+            % Compute angular deflection of current segment
+            theta_vec(ii) = M_vec(ii)*h/(E_vec(ii)*I);
+            
+            % Compute section arc length, curvature and
+            % strain
+            s1 = (h-ybar)*abs(theta_vec(ii));
+            kappa = theta_vec(ii)/s1;
+            epsilon = (kappa.*(ro-ybar))./(1+ybar*kappa);
+            
+            % Update modulus via gradient descent
+            [stress_eff,eta] = GetStress(abs(epsilon),E,0.08*E);
+            new_E = E_vec(ii)-eta*(E_vec(ii)-stress_eff/abs(epsilon));
+            
+            % Percent change in modulus (for convergence
+            % checking)
+            pct = abs((new_E-E_vec(ii))/E_vec(ii));
+            
+            % Update modulus guess
+            E_vec(ii) = new_E;
+            
+            % Increment
+            k = k+1;
+        end
+        
     end
     theta_vec = theta_vec + theta_vec_base;
     trials = trials + 1;
