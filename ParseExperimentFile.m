b681d387f32fa38cfdffc8ddc8f531e672243867
Nicholas Pacheco <nepacheco@wpi.edu>
2021-01-19 11:04:08 -0500

    Function to Parse and Experiment Excel File

---
+++ ParseExperimentFile.m
@@ -0,0 +1,15 @@
+function [force_vec, notch_mat] = ParseExperimentFile(file,n)
+%ParseExperimentFile - parses the NxM cell passed into a force vector and notch value
+%matrix
+force_index = 4;
+force_vec = [];
+notch1_index = 5;
+notch_mat = [];
+[N,M] = size(file);
+for i = 1:N
+    if (isnumeric(file{i,force_index}) && isnumeric(file{i,notch1_index}))
+        force_vec = [force_vec; file{i,force_index}];
+        notch_mat = [notch_mat; file{i,notch1_index:notch1_index+n}];
+    end
+end
+
