6a2c0a6ca3cb590e2bb6ee88a2c91ed3fd2691ab
Nicholas Pacheco <nepacheco@github.com>
2021-02-16 11:02:28 -0500

    Update PlotExperimentTrials.m
    Now labels data according to experiment

--- PlotExperimentTrials.m
+++ PlotExperimentTrials.m
@@ -1,61 +1,72 @@
-function p = PlotExperimentTrials(experimentFile,n)
-opts = detectImportOptions(experimentFile);
-opts.Sheet = 'AvgMeasurements';
-file = readcell(experimentFile,opts);
-[force_vec notch_data] = ParseExperimentFile(file,n);
+function p = PlotExperimentTrials(experimentFiles,n)
 
-trialForceList = cell(1,1);
-trialNotchDataList = cell(1,1);
-last = 1;
-for i = 1:length(force_vec)-1
-    if force_vec(i) > force_vec(i+1)
-        if last == 1
-            trialForceList = {force_vec(last:i)};
-            trialNotchDataList = {notch_data(last:i,:)};
-            last = i+1;
-        else
-            trialForceList = [trialForceList, {force_vec(last:i)}];
-            trialNotchDataList = [trialNotchDataList {notch_data(last:i,:)}];
-            last = i+1;
+experimentStr = "";
+line_type = {'-','--',':','-.'};
+figure('Name','Experiment Trials Plotted','WindowState','Maximize');
+for i = 1:length(experimentFiles)
+    force_vec = [];
+    notch_data = [];
+    backslash_indicies = strfind(experimentFiles(i),"\");
+    period_indicies = strfind(experimentFiles(i),".");
+    experimentStr = experimentStr + extractBetween(experimentFiles(i),backslash_indicies(end)+1,period_indicies(end)-1);
+    
+    opts = detectImportOptions(experimentFiles(i));
+    opts.Sheet = 'AvgMeasurements';
+    file = readcell(experimentFiles(i),opts);
+    [force_vec_i notch_data_i] = ParseExperimentFile(file,n);
+    force_vec = [force_vec; force_vec_i];
+    notch_data = [notch_data; notch_data_i];
+    
+    
+    trialForceList = cell(1,1);
+    trialNotchDataList = cell(1,1);
+    last = 1;
+    for f = 1:length(force_vec)-1
+        if force_vec(f) > force_vec(f+1)
+            if last == 1
+                trialForceList = {force_vec(last:f)};
+                trialNotchDataList = {notch_data(last:f,:)};
+                last = f+1;
+            else
+                trialForceList = [trialForceList, {force_vec(last:f)}];
+                trialNotchDataList = [trialNotchDataList {notch_data(last:f,:)}];
+                last = f+1;
+            end
+        elseif f == length(force_vec) - 1
+            trialForceList = [trialForceList, {force_vec(last:f+1)}];
+            trialNotchDataList = [trialNotchDataList {notch_data(last:f+1,:)}];
         end
-    elseif i == length(force_vec) - 1
-            trialForceList = [trialForceList, {force_vec(last:i+1)}];
-            trialNotchDataList = [trialNotchDataList {notch_data(last:i+1,:)}];
     end
-end
-
-
-
-figure('Name','Experiment Trials Plotted')
-hold on
-for i = 1:n+1
-    subplot(3,2,i)
+    
+    
     hold on
-    title_str = sprintf("Notch %d Deflection",i);
-    if i == n+1
-        title_str = sprintf("Total tip Deflection");
+    for p = 1:n+1
+        subplot(3,2,p)
+        hold on
+        title_str = sprintf("Notch %d Deflection",p);
+        if p == n+1
+            title_str = sprintf("Total tip Deflection");
+        end
+        title(title_str,'FontSize',16);
+        legend_entries = cell(1,size(trialForceList,2));
+        for t = 1:size(trialForceList,2)
+            disp_name = sprintf('Exp%d Trial %d',i,t);
+            plot(trialForceList{1,t},trialNotchDataList{1,t}(:,p),...
+                line_type{i},'LineWidth',2,'Marker','.',...
+                'DisplayName',disp_name);
+        end
+        xlabel('Force (N)','FontSize',12);
+        ylabel('Deflection (deg)','FontSize',12);
+        legend
+        hold off
     end
-    title(title_str,'FontSize',16);
-    legend_entries = cell(1,size(trialForceList,2));
-    for t = 1:size(trialForceList,2)
-        plot(trialForceList{1,t},trialNotchDataList{1,t}(:,i),...
-            '-','LineWidth',2,'Marker','.');
-        legend_entries(1,t) = {sprintf('Trial %d',t)};
-    end
-    xlabel('Force (N)','FontSize',12);
-    ylabel('Deflection (deg)','FontSize',12);
-    legend(legend_entries,'Location','southeast','FontSize',12);
-    hold off
 end
-
-SaveDestination = "ComparisonImages/TrialImages"
+SaveDestination = "ComparisonImages/TrialImages";
 destdirectory = sprintf("%s/",SaveDestination);
 if ~exist(destdirectory, 'dir')
     mkdir(destdirectory);
 end
-saveas(gcf,sprintf("%s/",SaveDestination,table2array(parameters(m,'ID')),wristType,experimentStr));
-saveas(gcf,sprintf("%s/",SaveDestination,SaveDestination,table2array(parameters(m,'ID')),wristType,experimentStr));
+saveas(gcf,sprintf("%s/%s_Trials.png",SaveDestination,experimentStr));
+saveas(gcf,sprintf("%s/%s_Trials.fig",SaveDestination,experimentStr));
 
-writematrix(rmse_total(:,:,m),...
-    sprintf("%s/PropertySet%d/RMSE_Values_%s_%s.xlsx",SaveDestination,table2array(parameters(m,'ID')),wristType,experimentStr));
 end
