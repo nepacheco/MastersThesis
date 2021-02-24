5097b1e8b14c98c734f6657708f55a573ab71bbf
Nicholas Pacheco <nepacheco@github.com>
2021-02-23 22:30:16 -0500

    Update ObjectiveFunction.m
    Can now take in a matrix for x and fixed issue where notches weren't being considered for total deflection

--- ObjectiveFunction.m
+++ ObjectiveFunction.m
@@ -1,26 +1,29 @@
 function rmse_summation = ObjectiveFunction(x,tipDeflection)
 %
 arguments
-    x (1,4) double
-    tipDeflection (1,1) logical = false
+    x (:,4) double
+    tipDeflection (1,1) logical = true
 end
-    
+
 %%
-load('ExperimentFiles.mat')
-E_lin = x(1);
-E_se = x(2);
-Strain_Lower = x(3);
-Mu = x(4);
-
-set = table(E_lin,E_se,Strain_Lower,Mu);
-valsTip = CompareModel('TipFirstTube',experimentFilesTip(3),true, set,'Force',3,'Plot',false);
-vals150 = CompareModel('150Tube2',experimentFiles150_2(1),true, set,'Force',3,'Plot',false);
-vals90 = CompareModel('90Tube',experimentFiles90(2),true,set,'Force',3,'Plot',false);
-
-if tipDeflection
-    rmse_summation = norm(valsTip(end,1,1)) + norm(vals150(end,1,1)) + norm(vals90(end,1,1));
-else
-    rmse_summation = norm(valsTip(1:end-1,1,1)) + norm(vals150(1:end-1,1,1)) + norm(vals90(1:end-1,1,1));
+numSets = size(x,1);
+rmse_summation = ones(numSets,1);
+for i = 1:size(x,1)
+    load('ExperimentFiles.mat')
+    E_lin = x(i,1);
+    E_se = x(i,2);
+    Strain_Lower = x(i,3);
+    Mu = x(i,4);
+    
+    set = table(E_lin,E_se,Strain_Lower,Mu);
+    valsTip = CompareModel('TipFirstTube',experimentFilesTip(3),true, set,'Force',3,'Plot',false);
+    vals150 = CompareModel('150Tube2',experimentFiles150_2(1),true, set,'Force',3,'Plot',false);
+    vals90 = CompareModel('90Tube',experimentFiles90(2),true,set,'Force',3,'Plot',false);
+    
+    if tipDeflection
+        rmse_summation(i) = norm(valsTip(:,1,1)) + norm(vals150(:,1,1)) + norm(vals90(:,1,1));
+    else
+        rmse_summation(i) = norm(valsTip(1:end-1,1,1)) + norm(vals150(1:end-1,1,1)) + norm(vals90(1:end-1,1,1));
+    end
 end
-
 end
