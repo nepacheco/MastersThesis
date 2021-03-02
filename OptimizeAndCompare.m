374b1768f6ff9ab398edd6f9a793f53b7545fb9b
Nicholas Pacheco <nepacheco@github.com>
2021-03-02 14:48:58 -0500

    Update OptimizeAndCompare.m

--- OptimizeAndCompare.m
+++ OptimizeAndCompare.m
@@ -25,22 +25,22 @@ OptimizeParameters('90Tube',experimentFiles90(2),true,false,numSets,...
     'strain_lower',strain_lowerRange,'mu',muRange);
 %%
 load('PropertySets.mat');
-valsTip = CompareModel('TipFirstTube',experimentFilesTip(3),true, PropertySets,'Force',3);
-vals150 = CompareModel('150Tube2',experimentFiles150_2(1),true, PropertySets,'Force',3);
-vals90 = CompareModel('90Tube',experimentFiles90(2),true,PropertySets,'Force',3);
+valsTip = CompareModel('TipFirstTube',experimentFilesTip(3),true, PropertySets,'Force',3,'Plot',false);
+vals150 = CompareModel('150Tube2',experimentFiles150_2(1),true, PropertySets,'Force',3,'Plot',false);
+vals90 = CompareModel('90Tube',experimentFiles90(2),true,PropertySets,'Force',3,'Plot',false);
 
 %%
 min_norm = [100,0];
 min_norm_noTip = [100, 0];
 for i = 1:size(valsTip,3)
-    if (norm(valsTip(:,1,i)) + norm(vals150(:,1,i)) + norm(vals90(:,1,i))) < min_norm(1)
-        min_norm(1) = norm(valsTip(:,1,i)) + norm(vals150(:,1,i)) + norm(vals90(:,1,i));
+    if (norm(vals150(:,1,i))) < min_norm(1)
+        min_norm(1) = norm(vals150(:,1,i));
         min_norm(2) = i;
     end
-    if(norm(valsTip(1:end-1,1,i)) + norm(vals150(1:end-1,1,i))+ norm(vals90(1:end-1,1,i))) < min_norm_noTip(1)
-        min_norm_noTip(1) = norm(valsTip(1:end-1,1,i)) + norm(vals150(1:end-1,1,i)) + norm(vals90(1:end-1,1,i));
-        min_norm_noTip(2) = i;
-    end
+%     if(norm(valsTip(1:end-1,1,i)) + norm(vals150(1:end-1,1,i))+ norm(vals90(1:end-1,1,i))) < min_norm_noTip(1)
+%         min_norm_noTip(1) = norm(valsTip(1:end-1,1,i)) + norm(vals150(1:end-1,1,i)) + norm(vals90(1:end-1,1,i));
+%         min_norm_noTip(2) = i;
+%     end
 end
 %%
 min_norm = [100,0];
@@ -56,15 +56,7 @@ for i = 1:size(valsTip,3)
     end
 end
 %%
-min_norm_noTip = [100, 0];
-for i = 1:size(valsTip,3)
-    if(norm(vals90(end,1,i))) < min_norm_noTip(1)
-        min_norm_noTip(1) = norm(vals90(end,1,i));
-        min_norm_noTip(2) = i;
-    end
-end
-%%
-expFiles = ["C:\Users\nickp\OneDrive - Worcester Polytechnic Institute (wpi.edu)\School Files\Thesis\ForceTest\02-06-2021_Experiment\02-06-2021_Results_Trial1.xlsx";
-    "C:\Users\nickp\OneDrive - Worcester Polytechnic Institute (wpi.edu)\School Files\Thesis\ForceTest\02-09-2021_Experiment\02-09-2021_Results_Trial1.xlsx"];
-vals150_1 = CompareModel('150Tube',expFiles,true,PropertySets([61,72],:))
+load('PropertySets.mat')
+load('ExperimentFiles.mat')
+vals150_1 = CompareModel('150Tube',experimentFiles150(end),true,PropertySets([116,187],:));
 
