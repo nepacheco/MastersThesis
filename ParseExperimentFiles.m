c692dd6d5ce22c9e0c80d6ce54aacada6d9d8f6c
Nicholas Pacheco <nepacheco@github.com>
2021-03-29 16:54:41 -0400

    Update ParseExperimentFiles.m
    Outputs data into single cell array

--- ParseExperimentFiles.m
+++ ParseExperimentFiles.m
@@ -1,10 +1,10 @@
-function [force_vec, notch_mat] = ParseExperimentFiles(experimentFiles,n)
+function experimentData = ParseExperimentFiles(experimentFiles,n)
 %ParseExperimentFile - parses the NxM cell passed into a force vector and notch value
 %matrix
 %   force vector output is Nx1 and notch_mat is Nxn where n is the number
 %   of notches in the tube.
 
-%% Parse Experiment Files
+% Parse Experiment Files
 numFiles = size(experimentFiles,1);
 force_cell = cell(1,numFiles);
 notch_cell = cell(1,numFiles);
@@ -18,25 +18,27 @@ for i = 1:numFiles
     opts = detectImportOptions(experimentFiles(i));
     opts.Sheet = 'AvgMeasurements';
     file = readcell(experimentFiles(i),opts);
-    [force_vec notch_data] = ParseExperimentFile(file,wrist.n);
+    [force_vec, notch_data] = ParseExperimentFile(file,n);
     
     force_cell(1,i) = {force_vec};
     notch_cell(1,i) = {notch_data};
     average(:,i) = mean(notch_data)';
 end
 
-%%
-function [force_vec, notch_mat] = ParseFile(file,n)
-    force_index = 4;
-    force_vec = [];
-    notch1_index = 5;
-    notch_mat = [];
-    [N,M] = size(file);
-    for i = 1:N
-        if (isnumeric(file{i,force_index}) && isnumeric(file{i,notch1_index}))
-            force_vec = [force_vec; file{i,force_index}];
-            notch_mat = [notch_mat; file{i,notch1_index:notch1_index+n}];
-        end
-    end
+experimentData = [force_cell, notch_cell,average,convertStringsToChars(experimentStr),numFiles];
+
 end
 
+function [force_vec, notch_mat] = ParseExperimentFile(file,n)
+force_index = 4;
+force_vec = [];
+notch1_index = 5;
+notch_mat = [];
+[N,M] = size(file);
+for v = 1:N
+    if (isnumeric(file{v,force_index}) && isnumeric(file{v,notch1_index}))
+        force_vec = [force_vec; file{v,force_index}];
+        notch_mat = [notch_mat; file{v,notch1_index:notch1_index+n}];
+    end
+end
+end
