caf087837104baa628ada91c41274637ba4b8873
Nicholas Pacheco <nepacheco@github.com>
2021-03-29 23:42:46 -0400

    Update OptimizeAndCompare.m
    Started to fix this, but didn't finish

--- OptimizeAndCompare.m
+++ OptimizeAndCompare.m
@@ -1,28 +1,29 @@
 clc;clear;close all;
 
 %%
-load('ExperimentFiles.mat')
+load('ExperimentFiles.mat');
+% Getting file data and creating wrists
+experimentData90 = ParseExperimentFiles(experimentFiles90(2),5);
+wrist90 = MakeWrist('90Tube',true);
+wrist90.use_non_linear = false;
 
-E_linRange = linspace(10E9,40E9,10);
-E_seRange = linspace(2.00E9,4.25E9,10);
-strain_lowerRange = linspace(0.005,0.03,10);
-muRange = linspace(0.1,0.35,10);
+experimentData150 = ParseExperimentFiles(experimentFiles150(6),5);
+wrist150 = MakeWrist('150Tube',true);
+wrist150.use_non_linear = false;
+
+experimentDataTipFirst = ParseExperimentFiles(experimentFilesTip(3),4);
+wristTipFirst = MakeWrist('TipFirstTube',true);
+wristTipFirst.use_non_linear = false;
+E_linRange = linspace(6E9,7E9,50);
+% % E_seRange = linspace(2.00E9,4.25E9,10);
+% % strain_lowerRange = linspace(0.005,0.03,10);
+muRange = linspace(0.05,0.35,10);
 numSets = 70;
 
-% Optimizing using Tip First Tube
-% OptimizeParameters('TipFirstTube',experimentFilesTip(3),true,false,numSets,...
-%     'E_lin',E_linRange,'E_se',E_seRange,...
-%     'strain_lower',strain_lowerRange,'mu',muRange);
-% 
-% % Optimizing using 150 Tube 2
-% OptimizeParameters('150Tube2',experimentFiles150_2(1),true,false,numSets,...
-%     'E_lin',E_linRange,'E_se',E_seRange,...
-%     'strain_lower',strain_lowerRange,'mu',muRange);
-
-% Optimizing using 150 Tube 2
-OptimizeParameters('90Tube',experimentFiles90(2),true,false,numSets,...
-    'E_lin',E_linRange,'E_se',E_seRange,...
-    'strain_lower',strain_lowerRange,'mu',muRange);
+OptimizeParameters(experimentDataTipFirst,experimentData90,experimentData150,...
+    wristTipFirst,wrist90,wrist150,...
+    false,numSets,...
+    'E_lin',E_linRange,'mu',muRange);
 %%
 load('PropertySets.mat');
 valsTip = CompareModel('TipFirstTube',experimentFilesTip(3),true, PropertySets,'Force',3,'Plot',false);
