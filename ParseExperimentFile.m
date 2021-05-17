ed7b4fc9c66ae72940e560ebb14e0ef0593de66e
Nicholas Pacheco <nepacheco@github.com>
2021-05-17 17:12:36 -0400

    Update ParseExperimentFile.m
    Will output tendon displacement now

--- ParseExperimentFile.m
+++ ParseExperimentFile.m
@@ -1,6 +1,8 @@
-function [force_vec, notch_mat] = ParseExperimentFile(file,n)
+function [force_vec, notch_mat, tendon_vec] = ParseExperimentFile(file,n)
 force_index = 4;
 force_vec = [];
+tendon_index = 3;
+tendon_vec = [];
 notch1_index = 5;
 notch_mat = [];
 [N,M] = size(file);
@@ -8,6 +10,7 @@ for v = 1:N
     if (isnumeric(file{v,force_index}) && isnumeric(file{v,notch1_index}))
         force_vec = [force_vec; file{v,force_index}];
         notch_mat = [notch_mat; file{v,notch1_index:notch1_index+n}];
+        tendon_vec = [tendon_vec; file{v,tendon_index}];
     end
 end
 end
