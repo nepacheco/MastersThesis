6c2e316b6fe31008a543043002004d6fcc2a009d
Nicholas Pacheco <nepacheco@wpi.edu>
2022-02-09 11:41:58 -0500

    Changed arguments to add more options

--- ParseExperimentFile.m
+++ ParseExperimentFile.m
@@ -1,16 +1,21 @@
-function [force_vec, notch_mat, tendon_vec] = ParseExperimentFile(file,n)
-force_index = 4;
+function [force_vec, notch_mat, tendon_vec] = ParseExperimentFile(file,n,options)
+arguments
+    file % the opened file
+    n = 5 % number of notches
+    options.force_index = 4 % the force index
+    options.tendon_index = 3 
+    options.notch1_index = 5
+end
 force_vec = [];
-tendon_index = 3;
 tendon_vec = [];
-notch1_index = 5;
 notch_mat = [];
 [N,M] = size(file);
 for v = 1:N
-    if (isnumeric(file{v,force_index}) && isnumeric(file{v,notch1_index}))
-        force_vec = [force_vec; file{v,force_index}];
-        notch_mat = [notch_mat; file{v,notch1_index:notch1_index+n}];
-        tendon_vec = [tendon_vec; file{v,tendon_index}];
+    if (isnumeric(file{v,options.force_index}) ...
+           && isnumeric(file{v,options.notch1_index}))
+        force_vec = [force_vec; file{v,options.force_index}];
+        notch_mat = [notch_mat; file{v,options.notch1_index:options.notch1_index+n}];
+        tendon_vec = [tendon_vec; file{v,options.tendon_index}];
     end
 end
 end
