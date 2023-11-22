cc73f8eaabeb814e90fb33b98812632a73f5505d
Nicholas Pacheco <nepacheco@wpi.edu>
2023-11-22 17:15:39 -0500

    Updates for determining notch cutting pattern

--- Notch Geometry Scripts/Tip_First_Bending.m
+++ Notch Geometry Scripts/Tip_First_Bending.m
@@ -1,26 +1,44 @@
 %% Initial Conditions
 clc; clear; close all;
-maxBendPerNotch = 30; % [deg] - angle per notch
+% Desired theta setpoint for each notch.
+% This creates a distance during tendon displacement where the notches in
+% the wrist will take on these values. 
+theta_des = [10 15 20 25 30].*pi/180;  %[rad]
+
+n = 5; % number of notches
+wristLength = 15e-3; %[m] Length of wrist.
+maxBendPerNotch = [10 15 20 25 30]; % [deg] - maximum bending angle per notch
 od = 1.62E-3; % [m] - outer diameter of tube
 id = 1.4E-3; % [m] - inner diameter of tube
-n = 5; % number of notches
+phi = zeros(n,1);
 maxG = 0.8642*od; % [m] - Max depth which we assign to notch n.
-[~,h,u] = GetNotchSynthesis(maxBendPerNotch,maxG,od,id,n);
-E_lin = 40E9; % [N/m^2] - Elastic Modulus of Nitinol
-E_se = 0.08*E_lin; % [N/m^2] - Slope of Super Elastic Region for Nitinol
-mu = 0.2; % coefficient of friction for capstan
+cutType = 'off-axis';
+[~,h,u] = GetNotchSynthesis(maxBendPerNotch,maxG,od,id,'CutType',cutType,...
+    'L',wristLength);
+maxForce = 1.75; %[N]
+
+% Based on Pacheco et al. JMRR. 2021.
+E_lin = 10E9; % [N/m^2] - Elastic Modulus of Nitinol
+E_se = 3e9; % [N/m^2] - Slope of Super Elastic Region for Nitinol
+mu = 0.13; % coefficient of friction for capstan
+strain_lower = 0.028;
+% Default Values
+% E_lin = 40E9; % [N/m^2] - Elastic Modulus of Nitinol
+% E_se = 0.08*E_lin; % [N/m^2] - Slope of Super Elastic Region for Nitinol
+% mu = 0.2; % coefficient of friction for capstan
+% strain_lower = 0.02;
 
 
 %% Tip First Bending but incorporating different elastic modulus per notch
 
 % Get the resulting ybar and I from our chosen cut depth
-[maxYbar, minI] = GetNeutralAxis(od/2, id/2, maxG);
-% Desired theta for each notch. If n changes change the size of this vector
-theta_des = [30 30 30 30 30].*pi/180;
+[maxYbar, minI] = GetNeutralAxis(od/2, id/2, maxG,'CutType',cutType);
 % Determin the approximated linear elastic modulus
-[stress, strain, E] = GetStrainInformation(theta_des(5), h, od/2, maxYbar,E_lin,E_se);
+[strainFin, stressFin, E] = GetStrainInformation(theta_des(n), h(n), od/2, maxYbar,...
+    'E_lin',E_lin, 'E_se',E_se,'strainLower',strain_lower);
 % Determine force necessary to achieve desired bend at specifically notch n
-Fdesired = theta_des(n)*E*minI/(h*(id/2 + maxYbar)*exp(-mu*sum(theta_des)));
+% Inverting equation 12 in Pacheco et al. JMRR. 2021
+Fdesired = theta_des(n)*E*minI/(h(n)*(id/2 + maxYbar)*exp(-mu*sum(theta_des)));
 
 
 % *** FOR DATA COLLECTION ***
@@ -37,10 +55,14 @@ for i = 1:n
     error = 10;
     steps = 0;
     while(abs(error) > 1E-9)
-        % Same process as up above
-        [ybar, I] = GetNeutralAxis(od/2, id/2, g);
-        [stress, strain, E] = GetStrainInformation(theta_des(i), h, od/2, ybar,E_lin,E_se);
-        Fpw = theta_des(i)*E*I/(h*(id/2 + ybar)*exp(-mu*sum(theta_des(1:i))));
+        % Same process as up above to determine how much force would be
+        % necessary to pull notch (i) given its current cut depth, theta
+        % target, and other wrist parameters.
+        [ybar, I] = GetNeutralAxis(od/2, id/2, g,'CutType',cutType);
+        [strain, stress, E] = GetStrainInformation(theta_des(i), h(i), od/2, ybar,...
+                'E_lin',E_lin, 'E_se',E_se,'strainLower',strain_lower);
+        Fpw = theta_des(i)*E*I/(h(i)*(id/2 + ybar)*exp(-mu*sum(theta_des(1:i))));
+        
         % Need our pullwire force to get close to our desired force
         error = Fpw - Fdesired;
         g = g + stepsize*error;
@@ -63,31 +85,22 @@ end
 g_vec
 
 % *** TESTING THE ABOVE RESULTS ***
-T_0 = eye(4);
-N = n;                 % Number of notches
-r_o = od/2;         % Tube Outer Radius [m]
-r_i = id/2;         % Tube Inner Radius [m]
-h_w = linspace(h,h,N);    % Notch Width Vector [m]
-h_c = linspace(h,h,N);    % Notch Collision Width Vector (<h) [m]
-g = g_vec;     % Notch depth vector which we just determined [m]
-c = (u).*ones(N,1);             % Notch spacing vector [m]
-b = u;             % Distal offset [m]
-FOS = 1;                % Factor of Safety
 
 % Create instance of wrist object
-sheath = JoshWrist();
-sheath.ConstructWrist('T_0',T_0,'g',g,'c',c,'b',b,'h',h_w,'h_c',h_c,...
-    'r_o',r_o,'r_i',r_i,'n',N,'material','nitinol','plotkin',false,'verbose',false);
+sheath = Wrist(od,id,n,h,phi,u,g_vec,'CutType',cutType,'Name','TestWrist');
+sheath.E_lin = E_lin;
+sheath.E_se = E_se;
+sheath.mu = mu;
+sheath.strain_lower = strain_lower;
 
-sheath = Wrist(od,id,n,h*ones(n,1),zeros(n,1),c,g_vec,'CutType','off-axis');
 
 % **** PLOTTING NOTCH ANGLE WRT FORCE **********
 % sheath.FindMaxForce(1,5);
 des_points = zeros(n,2);
 theta_last = zeros(n,1);
-points = 100; % how many points to plot
+points = 200; % how many points to plot
 theta_mat = zeros(n,points); % Initializing empty array to store values
-F = linspace(0,5,points); % Getting list of forces (x axis values)
+F = linspace(0,maxForce,points); % Getting list of forces (x axis values)
 for i = 1:points
 %     sheath.GetKinematicsForce(F(i)); % Updating tube position
     sheath.fwkin([F(i);0;0]);
@@ -96,7 +109,7 @@ for i = 1:points
     % *** DATA COLLECTION TO SEE IF WE CROSSED DESIRED ANGLE VALUE ***
     diff = (theta_last-theta_des') .* (sheath.theta-theta_des'); 
     for k = 1:n % check each notch individually
-        if diff(k) <= 0 % We have crosed over the desired angle
+        if diff(k) <= 0% We have crosed over the desired angle
             % Grab the point that was closest to the desired angle
             if abs(theta_last(k)-theta_des(k)) < abs(sheath.theta(k) - theta_des(k))
                 des_points(k,1) = F(i-1);
@@ -113,7 +126,7 @@ end
 theta_mat = theta_mat.*(180/pi); % Convert to deg
 close all;
 figure();
-plot(F,theta_mat);
+plot(F,theta_mat,'LineWidth',2);
 title(sprintf("Notch angles with respect to force applied at tendon\ndesigned to close at the same time"),'FontSize',16)
 xlabel("Force (N)",'FontSize',14)
 ylabel("Angle (rad)",'FontSize',14)
@@ -128,7 +141,9 @@ for (i = 1:n)
         sprintf("theta %u",i));
 end
 hold on
+% Plot our desired force and the desired theta for each wrist
 stem(Fdesired.*ones(1,n),theta_des.*180/pi,'ok','MarkerSize',10)
+% Plot the locations where our wrist actually hit the desired theta.
 stem(des_points(:,1),des_points(:,2),'.r','MarkerSize',10);
 text(Fdesired.*ones(1,n)-0.01,theta_des.*180/pi,labels,'VerticalAlignment','bottom',...
     'HorizontalAlignment','right');
@@ -149,5 +164,5 @@ legend(legendLabels,'Location','northwest','FontSize',12);
 % disp(s)
 
 disp(['Notch Width (w): ' num2str(g_vec) ' m']);
-disp(['Notch Height (h) : ' num2str(h) ' m']);
-disp(['Notch Height (u) : ' num2str(u) ' m']);
+disp(['Notch Height (h) : ' num2str(h') ' m']);
+disp(['Notch Height (u) : ' num2str(u') ' m']);
