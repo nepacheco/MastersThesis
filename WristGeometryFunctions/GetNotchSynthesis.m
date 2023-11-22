d755b4941e556f682ddcd195b267ff3215d1621d
Nicholas Pacheco <nepacheco@wpi.edu>
2023-11-22 15:54:17 -0500

    Added argument validation and can now choose between on-axis and off-axis cutting

--- WristGeometryFunctions/GetNotchSynthesis.m
+++ WristGeometryFunctions/GetNotchSynthesis.m
@@ -1,38 +1,34 @@
-function [w,h,u] = GetNotchSynthesis(maxBendingAngle,w,OD,ID,n)
+function [w,h,u] = GetNotchSynthesis(maxBendingAngle,w,OD,ID,opts)
 %GETNOTCHSYNTHESIS Returns cut depth, height, and uncut spacing
 %   Determines the notch geometries to achieve a certain amount of bending
 %   per angle based on the cut depth, outer diameter, and inner diameter of
 %   the tube. Based on Alex's notchsynthesis.m script
+arguments
+    maxBendingAngle (1,1) double  %[deg] the maximum bending angle for the notch
+    w (1,1) double  %[m] The assumed cut depth of each notch
+    OD (1,1) double  %[m] outer diameter of the tube in meters
+    ID (1,1) double  %[m] Inner diameter of the tube in meters
+    opts.L (1,1) double = 15e-3  % [m] total length of the section
+    opts.n (1,1) double = 1 % the number of notches
+    opts.CutType char {mustBeMember(opts.CutType,{'on-axis','off-axis'})} = 'on-axis'  % Cutting pattern used for the notches
+end
 
 % Script to calculate the notch geometry
 
 % Define the total notch + uncut section length:
-L = 15e-3; %[m]
+L = opts.L; %[m]
 % Define the desired maximum bending angle for the wrist
-thetaMax = n*maxBendingAngle * pi / 180; % [radians]
+thetaMax = maxBendingAngle * pi / 180; % [radians]
 %% You should not need to change anything from the code below
+
+
 
 ro = OD/2;         % [m] tube outer radius
 ri = ID/2;         % [m] tube inner radius
 
-t = w - ro;   % [m]
-phio = 2 * acos(t / ro); % [rad]
-phii = 2 * acos(t / ri); % [rad]
-ybaro = (4 * ro * (sin(0.5 * phio)) ^ 3)/ (3 * (phio - sin(phio)));
-ybari = (4 * ri * (sin(0.5 * phii)) ^ 3)/ (3 * (phii - sin(phii)));
-Ao = ( (ro ^ 2) * ( phio - sin(phio))) / 2;
-Ai = ( (ri ^ 2) * ( phii - sin(phii))) / 2;
-ybar1 = (ybaro * Ao - ybari * Ai) / (Ao - Ai);
+[ybar, ~] = GetNeutralAxis(ro, ri, w,'CutType',opts.CutType);
 
-fy = @(r,theta) (r.*cos(theta)); 
-fA = @(r,theta) (1);
-phi = @(r) acos(t./r);
-A = integral2( @(r,theta) r.*fA(r,theta), ri,ro,@(c)-phi(c),@(c)phi(c),'AbsTol',1e-12,'RelTol',1e-12 );
-ybar = 1/A*integral2( @(r,theta) r.*fy(r,theta),ri,ro,@(c)-phi(c),@(c)phi(c),'AbsTol',1e-12,'RelTol',1e-9 );
-
-
-
-h = thetaMax * (ro+ybar) / n;
+h = thetaMax * (ro+ybar);
 u = L/n - h;
 %d = h/(h+u);
 disp(['Notch Width (w): ' num2str(w*1e3) ' mm']);
