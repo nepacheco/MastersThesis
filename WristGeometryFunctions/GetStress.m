24350b5b784c6393f47e93df8015eb3c727ce2f8
Nicholas Pacheco <nepacheco@wpi.edu>
2023-11-22 15:54:46 -0500

    Added function validation. user can input linear, super elastic, and strain upper/lower

--- WristGeometryFunctions/GetStress.m
+++ WristGeometryFunctions/GetStress.m
@@ -1,12 +1,20 @@
-function [stress,eta] = GetStress(strain, E_lin, E_se)
+function [stress,eta] = GetStress(strain, E_lin, E_se, opts)
 % GETSTRESS - returns the stress of a single notch based on stress strain
 % curve of nitinol
-strain_lower = 0.02;
-strain_upper = 0.1;
+arguments
+    strain (1,1) double  % approximate strain experienced by notch
+    E_lin  (1,1) double  % the estimated linear elastic modulus of the material
+    E_se   (1,1) double  % The estimated super elastic modulus of the material
+    opts.strain_lower  (1,1) double = 0.028  % The strain threshold where material becomes super elastic
+    opts.strain_upper (1,1) double = 0.1  % the strain threshold where material becomes plastic. 
+end
+
+strain_lower = opts.strain_lower;
+strain_upper = opts.strain_upper;
 
 sigma = @(e) (e<strain_lower).*e*E_lin+...
-    (e >= strain_lower && e < strain_upper)*((e-strain_lower)*.08*E_lin+strain_lower*E_lin)+...
-    (e >= strain_upper)*((1.0E9)*exp(-.01/(e-strain_upper))+strain_lower*1*E_lin+(strain_upper-strain_lower)*.08*E_lin);
+    (e >= strain_lower && e < strain_upper)*((e-strain_lower)*E_se+strain_lower*E_lin)+...
+    (e >= strain_upper)*((1.0E9)*exp(-.01/(e-strain_upper))+strain_lower*1*E_lin+(strain_upper-strain_lower)*E_se);
 stress = abs(sigma(strain));
 eta_fun = @(e) (e<strain_lower).*.5+...
     (e >= strain_lower && e < strain_upper)*1+...
